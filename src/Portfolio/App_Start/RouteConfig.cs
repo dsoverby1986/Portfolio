12d42b51d234db87b6e31692721281a514a99860
Shane Overby <dsoverby1986@gmail.com>
2015-08-14 11:15:28 -0400

    Route Config Url LowerCase
    set urls to be lowercase in route config

--- src/Portfolio/App_Start/RouteConfig.cs
+++ src/Portfolio/App_Start/RouteConfig.cs
@@ -24,6 +24,8 @@ namespace Portfolio
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.LowercaseUrls = true;
         }
     }
 }
